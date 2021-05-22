c262b705c45bee40de5210d8ae7b2a30214587a7
PashaEfimchik <61446424+PashaEfimchik@users.noreply.github.com>
2021-05-22 13:42:09 +0300

    Update Unit3.cpp

--- Lab_2/Unit3.cpp
+++ Lab_2/Unit3.cpp
@@ -15,16 +15,16 @@ __fastcall TForm3::TForm3(TComponent* Owner)
 }
 //---------------------------------------------------------------------------
 
-#include <fstream>   // для записи и чтения данных из файла
-#include <vector>    // хранит шаблон вектора
+#include <fstream>   
+#include <vector>    
 #include <string>
-#include <map>       // подключение STL контейнера
+#include <map>       
 
 using namespace std;
 
 const int MaxWardCapacity = 4;
 
-struct Patient   // структура данных
+struct Patient   
 {
     AnsiString Name;
 	AnsiString Sex;
@@ -33,10 +33,10 @@ struct Patient   // структура данных
 	int Ward;
 };
 
-vector<Patient> Database;  // объявление вектора с типом данных структры
+vector<Patient> Database;  
 
 AnsiString FormatPatient(int i, AnsiString _name, AnsiString _sex, AnsiString _age, AnsiString _diag){
-	return Database[i].Name + _name +            // инициализация данных вектора структуры Patient
+	return Database[i].Name + _name +            
 			Database[i].Sex + _sex +
 			IntToStr(Database[i].Age) + _age +
 			Database[i].Diagnosis + _diag +
@@ -47,41 +47,41 @@ AnsiString FormatPatient(int i, AnsiString _add){
 	return FormatPatient(i, _add, _add, _add, _add);
 }
 
-bool compWard(int a, int b){       // функция сравнения по палатам
+bool compWard(int a, int b){       
 	if(Database[a].Ward != Database[b].Ward)
 		return Database[a].Ward < Database[b].Ward;
 	return Database[a].Name < Database[b].Name;
 }
-bool compDiagnosis(int a, int b){     // функция сравнения по диагнозу болезни
+bool compDiagnosis(int a, int b){     
 	if(Database[a].Diagnosis != Database[b].Diagnosis)
 		return Database[a].Diagnosis < Database[b].Diagnosis;
-	if(Database[a].Ward != Database[b].Ward)   // в одной палате не могут быть люди с разными болезнями
+	if(Database[a].Ward != Database[b].Ward)   
 		return Database[a].Ward < Database[b].Ward;
 	return Database[a].Name < Database[b].Name;
 }
-bool compName(int a, int b){     // функция сравнения по имени
+bool compName(int a, int b){     
 	if(Database[a].Name != Database[b].Name)
 		return Database[a].Name < Database[b].Name;
 	return Database[a].Ward < Database[b].Ward;
 }
 
-vector<int> ListInds;  // объявление вектора с типом данных int списка
+vector<int> ListInds; 
 
-void ReWrite(){   // функция перезаписи списка
-	ListInds.clear();   // очищение списка
+void ReWrite(){   
+	ListInds.clear();   
 	if(Form3->RadioButtonList->ItemIndex == 0)
 		for(int i = 0; i < Database.size(); i++)
 			if(Database[i].Ward != 0)
-				ListInds.push_back(i);  // добавление элементов в конец вектора
+				ListInds.push_back(i);  
 	switch(Form3->ComboBoxSearch->ItemIndex){
 		case 0:
-			sort(ListInds.begin(), ListInds.end(), compWard); // сортировка по палатам
+			sort(ListInds.begin(), ListInds.end(), compWard); 
 			break;
 		case 1:
-			sort(ListInds.begin(), ListInds.end(), compDiagnosis); // сортировка по диагнозу
+			sort(ListInds.begin(), ListInds.end(), compDiagnosis);
 			break;
 		case 2:
-			sort(ListInds.begin(), ListInds.end(), compName); // сортировка по имени
+			sort(ListInds.begin(), ListInds.end(), compName); 
 			break;
 	}
 
@@ -92,10 +92,8 @@ void ReWrite(){   // функция перезаписи списка
 		Database[ListInds[i]].Name.LowerCase().Pos(Form3->EditSearch->Text.Trim().LowerCase()) != 0 ||
 		Database[ListInds[i]].Diagnosis.LowerCase().Pos(Form3->EditSearch->Text.Trim().LowerCase()) != 0 ||
 		IntToStr(Database[ListInds[i]].Ward).Pos(Form3->EditSearch->Text.Trim().LowerCase()) != 0)
-		 // проверка на введенные данные
 		{
 			Form3->ListBox->Items->Add(FormatPatient(ListInds[i], (AnsiString)(char)9 + " | ", " | ", " | ", (AnsiString)(char)9 + " | "));
-		// если данные введены, то они записываются в список и отображаются в ListBox
 		}
 }
 
@@ -105,26 +103,26 @@ const char lend = '\n';
 const AnsiString SexM = "Men";
 const AnsiString SexW = "Women";
 
-Patient getPatient(){  // добавление данных
-	Patient tmp;  // переменная структуры
+Patient getPatient(){  
+	Patient tmp;  
 
-	tmp.Name = Form3->EditName->Text.Trim(); // получение имени
-	if(tmp.Name.Length() == 0){    // проверка на наличие данных
+	tmp.Name = Form3->EditName->Text.Trim(); 
+	if(tmp.Name.Length() == 0){    
 		ShowMessage("Input Name");
 		throw 0;
 	}
 
-	if(Form3->RadioGroupSex->ItemIndex == 0) // определение пола
+	if(Form3->RadioGroupSex->ItemIndex == 0) 
 		tmp.Sex = SexM;
 	else if(Form3->RadioGroupSex->ItemIndex == 1)
 		tmp.Sex = SexW;
 	else {
-		ShowMessage("Wrong select sex"); // ошибка выбора пола
+		ShowMessage("Wrong select sex"); 
 		throw 0;
 	}
 
 	try {
-		tmp.Age = StrToInt(Form3->EditAge->Text);  // определение возраста
+		tmp.Age = StrToInt(Form3->EditAge->Text);  
 		if(tmp.Age <= 18)
 			throw 0;
 		if(tmp.Age > 110)
@@ -134,25 +132,25 @@ Patient getPatient(){  // добавление данных
 			throw 0;
 	}
 
-	tmp.Diagnosis = Form3->EditDiag->Text.Trim(); // определение диагноза
+	tmp.Diagnosis = Form3->EditDiag->Text.Trim(); 
 	if(tmp.Diagnosis.Length() == 0){
 		ShowMessage("Wrong diagnosis");
 		throw 0;
 	}
 
 	try {
-		tmp.Ward = StrToInt(Form3->EditWard->Text);  // определение палаты
+		tmp.Ward = StrToInt(Form3->EditWard->Text);  
 	} catch (...) {
 		ShowMessage("Wrong ward");
 		throw 0;
 	}
 
-	return tmp;   // получение переменной со значениями структыру
+	return tmp;  
 }
 
 void __fastcall TForm3::ButtonSaveChangesClick(TObject *Sender)
 {
-		if(CheckBoxAddPatient->Checked) {   // проверка включения флага
+		if(CheckBoxAddPatient->Checked) {   
 		Patient tmp;
 		bool flag = true;
 		try {
@@ -161,13 +159,12 @@ void __fastcall TForm3::ButtonSaveChangesClick(TObject *Sender)
 			Form3->RadioButtonList->ItemIndex = 0;
 					map<int, int> s;  //
 					for(int i = 0; i < Database.size(); i++)
-						s[Database[i].Ward] += 1; // добавление данных
+						s[Database[i].Ward] += 1; 
 					if(s[tmp.Ward] == MaxWardCapacity)
 						throw (AnsiString)"Invalid input";
 					for(int i = 0; i < Database.size(); i++)
 						if(Database[i].Ward == tmp.Ward && Database[i].Sex != tmp.Sex)
 							throw (AnsiString)"There can only be one sex in one ward";
-							// проверка на разные пола в одной палате
 				}
 			 catch (AnsiString err) {
 				ShowMessage(err);
@@ -177,9 +174,9 @@ void __fastcall TForm3::ButtonSaveChangesClick(TObject *Sender)
 			flag = false;
 		}
 		if(flag){
-			Database.push_back(tmp);   // добавление данных в конец структуры
-			RadioButtonListClick(Sender);  // получение адреса пацинтов
-			CheckBoxAddPatientClick(Sender);   // получение адреса флага
+			Database.push_back(tmp);   
+			RadioButtonListClick(Sender);  
+			CheckBoxAddPatientClick(Sender);   
 		}
 	} else {
 		if(ListBox->ItemIndex != -1){
@@ -208,9 +205,9 @@ void __fastcall TForm3::ButtonSaveChangesClick(TObject *Sender)
 				flag = false;
 			}
 			if(flag){
-				Database[ListInds[p]] = tmp; // изменение данных
+				Database[ListInds[p]] = tmp; 
 				RadioButtonListClick(Sender);
-				CheckBoxAddPatient->Checked = true;  // включение флага
+				CheckBoxAddPatient->Checked = true;  
 			}
 		}
 	}
@@ -219,9 +216,9 @@ void __fastcall TForm3::ButtonSaveChangesClick(TObject *Sender)
 //---------------------------------------------------------------------------
 void __fastcall TForm3::ButtonDeleteClick(TObject *Sender)
 {
-	if(ListBox->ItemIndex != -1){      // проверка на наличие элементов списка
+	if(ListBox->ItemIndex != -1){      
 		Database.erase(Database.begin() + ListInds[ListBox->ItemIndex]);
-		// перезапись списка без выбранного элемента
+		
 		RadioButtonListClick(Sender);
 	}
 }
@@ -234,10 +231,10 @@ void __fastcall TForm3::ButtonSearchClick(TObject *Sender)
 void __fastcall TForm3::ListBoxClick(TObject *Sender)
 {
 	if(ListBox->ItemIndex != -1){
-		int p = ListBox->ItemIndex;  // создание новой переменной идлекса списка
+		int p = ListBox->ItemIndex;  
 		int i = ListInds[p];
-		CheckBoxAddPatient->Checked = false;   // если флаг добаления пациентов выключен
-		EditName->Text = Database[i].Name;     // поля для записи инициализируются данными из списка
+		CheckBoxAddPatient->Checked = false;   
+		EditName->Text = Database[i].Name;     
 		if(Database[i].Sex == SexM)
 			RadioGroupSex->ItemIndex = 0;
 		else if(Database[i].Sex == SexW)
@@ -248,16 +245,16 @@ void __fastcall TForm3::ListBoxClick(TObject *Sender)
 		EditDiag->Text = Database[i].Diagnosis;
 		EditWard->Text = IntToStr(Database[i].Ward);
 		RadioButtonListClick(Sender);
-		ListBox->ItemIndex = p; // присваивание нового индекса списку
+		ListBox->ItemIndex = p; 
 	}
 }
 //---------------------------------------------------------------------------
 void __fastcall TForm3::ButtonOpenDialogClick(TObject *Sender)
 {
 	if (OpenDialog->Execute()) {
-		ifstream fin(OpenDialog->FileName.c_str());  // создание переменной для чтения из файла
+		ifstream fin(OpenDialog->FileName.c_str());  
 		if(!fin){
-			ShowMessage("File is not founded");  // если файл не найден, вызывается сообщение
+			ShowMessage("File is not founded");  
 			return;
 		}
 		Database.clear();
@@ -285,7 +282,7 @@ void __fastcall TForm3::ButtonOpenDialogClick(TObject *Sender)
 			int p = 2, i = 2;
 
 			while(str[i] != delta) i++;
-			tmp.Name = str.SubString(p, i - p);  // показать имя с i-ого по i-p символ
+			tmp.Name = str.SubString(p, i - p);  
 			i++;
 			p = i;
 
@@ -309,8 +306,7 @@ void __fastcall TForm3::ButtonOpenDialogClick(TObject *Sender)
 			i++;
 			p = i;
 
-			Database.push_back(tmp); // добавление данных в конец структуры
-
+			Database.push_back(tmp); 
 		}
 		ButtonFlushListClick(Sender);
 		Form3->RadioButtonList->ItemIndex = 0;
@@ -322,15 +318,15 @@ void __fastcall TForm3::ButtonOpenDialogClick(TObject *Sender)
 void __fastcall TForm3::ButtonSaveDialogClock(TObject *Sender)
 {
 	if (SaveDialog->Execute()) {
-		ofstream fout(SaveDialog->FileName.c_str());   // создание переменной для записи в файл
+		ofstream fout(SaveDialog->FileName.c_str());  
 		if(!fout){
-			ShowMessage("File is not founded"); // если файл не открыт, вызывается сообщение
+			ShowMessage("File is not founded"); 
 			return;
 		}
 		for(int i = 0; i < Database.size(); i++) {
-			fout << delta << FormatPatient(i, delta) << delta << lend; // запись данных в файл
+			fout << delta << FormatPatient(i, delta) << delta << lend; 
 		}
-	fout.close();  // закрытие файла
+	fout.close();  
 	}
 }
 //---------------------------------------------------------------------------
@@ -341,7 +337,7 @@ void __fastcall TForm3::RadioButtonListClick(TObject *Sender)
 //---------------------------------------------------------------------------
 void __fastcall TForm3::ButtonFlushListClick(TObject *Sender)
 {
-	EditSearch->Text = ""; // обнуление введенных для поиска данных
+	EditSearch->Text = ""; 
 	ReWrite();
 }
 //---------------------------------------------------------------------------
