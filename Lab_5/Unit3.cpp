f4a28e1d9956d7292d69148c43e84485671477d0
PashaEfimchik <61446424+PashaEfimchik@users.noreply.github.com>
2021-05-22 13:56:43 +0300

    lab_5

--- Lab_5/Unit3.cpp
+++ Lab_5/Unit3.cpp
@@ -21,15 +21,15 @@ __fastcall TForm3::TForm3(TComponent* Owner)
 
 void Redraw()
 {
-	Form3->ListBox->Clear();    // очистка трех listbox
+	Form3->ListBox->Clear();    
 	Form3->ListBoxPos->Clear();
 	Form3->ListBoxNeg->Clear();
 
-	for(auto it = in.begin(); it != in.end(); ++it)   // заполнение главного ListBox
+	for(auto it = in.begin(); it != in.end(); ++it)   
 		Form3->ListBox->Items->Add(IntToStr(in[it]));
-	for(auto it = a.begin(); it != a.end(); ++it)     // заполнение для отрицательных
+	for(auto it = a.begin(); it != a.end(); ++it)    
 		Form3->ListBoxNeg->Items->Add(IntToStr(a[it]));
-	for(auto it = b.begin(); it != b.end(); ++it)      // заполнение для положительных
+	for(auto it = b.begin(); it != b.end(); ++it)      
 		Form3->ListBoxPos->Items->Add(IntToStr(b[it]));
 }
 
@@ -39,27 +39,27 @@ bool rule (const int & x) {
 
 void __fastcall TForm3::ButtonAddClick(TObject *Sender)
 {
-	in.push_back(((rand()%2)*(-2)+1) * (1+rand()%100));  // генерация рандомных чисел от -100 до 100
+	in.push_back(((rand()%2)*(-2)+1) * (1+rand()%100));  
 	Redraw();
 }
 //---------------------------------------------------------------------------
 void __fastcall TForm3::ButtonMakeClick(TObject *Sender)
 {
-	a.clear();   // очистка первого ListBox
-	b.clear();   // очистка второго ListBox
-	in.split(a, b, rule);   // разделение главного списка на два списка
-	Redraw();   // перезапись списков
+	a.clear();  
+	b.clear();  
+	in.split(a, b, rule);   
+	Redraw();  
 }
 //---------------------------------------------------------------------------
 void __fastcall TForm3::ButtonDelClick(TObject *Sender)
 {
-	if(in.begin() != in.end()) // проверка на то является ли первый элемент списка последним
-		in.pop_front();  // если нет, то удаляется первый элемент списка
-	Redraw();       // перезапись списка
+	if(in.begin() != in.end()) 
+		in.pop_front(); 
+	Redraw();      
 }
 //---------------------------------------------------------------------------
 void __fastcall TForm3::Button1Click(TObject *Sender)
 {
-Close(); // закрытие формы
+Close();
 }
 //---------------------------------------------------------------------------
